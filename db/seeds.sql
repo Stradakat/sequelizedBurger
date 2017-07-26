f063d39c72665a26047022ed912bdeace8e08116
Rick Felix <Felirick@gmail.com>
2017-07-26 14:05:56 -0700

    Changes
    Draft 3

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Mac", false, "2016-11-15 04:22:00"),
+  ("Whuper", false, "2016-11-05 05:20:13"),
+  ("Cheeseburger", false, "2016-11-05 09:23:02"),
+  ("Baconsaurus", false, "2017-07-05 14:20:00"),
+  ("Quarter Pounder", false, "2017-11-05 18:23:01"),
+  ("McRegal", false, "2016-11-05 06:20:14"),
+  ("Double Trouble", true, "2016-11-05 15:20:16");
